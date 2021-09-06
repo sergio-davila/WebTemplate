b059b1d0b7c21bdc16c6095b769b949d7f9e253e
Sergio Alexander Davila <78042215+sergio-davila@users.noreply.github.com>
2021-09-05 23:53:05 -0500

    added a gitignore

---
+++ src/main/java/com/example/alesite/AleSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.alesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AleSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alesite/AleSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
